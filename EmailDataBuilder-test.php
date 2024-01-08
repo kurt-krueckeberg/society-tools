c31dbbf9db9877ae6b23882cca66630fc19ed82b
kurt krueckeberg <kurt.krueckeberg@gmail.com>
2024-01-08 09:43:43 -0500

    $1

--- EmailDataBuilder-test.php
+++ EmailDataBuilder-test.php
@@ -1,5 +1,6 @@
 <?php
 declare(strict_types=1);
+use namespace SocietyTools\{EmailDataBuilder};
 
 $csvname = "csv-member-list.csv";
 
@@ -7,7 +8,7 @@ $file = new SplFileObject($csvname, "r");
 
 $file->setFlags(\SplFileObject::READ_CSV| \SplFileObject::READ_AHEAD | \SplFileObject::SKIP_EMPTY | \SplFileObject::DROP_NEW_LINE);
 
-$ebuilder = new EmailListBuilder();
+$ebuilder = new EmailDataBuilder();
 
 foreach ($file as $arr)
      $ebuilder($arr);  
