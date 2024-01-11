d4271270b9390daf3a4fe02531490767432ff73c
kurt krueckeberg <kurt.krueckeberg@gmail.com>
2024-01-11 14:07:16 -0500

    $1

--- create-zoom-data.php
+++ create-zoom-data.php
@@ -11,7 +11,7 @@ function is_allen_county(int $zip) : bool
 // sorted static array of Allen county zip codes.
 static $allenzips= array(46704,46706,46723,46733,46741,46743,46745,46748,46765,46773,46774,46777,46783,46788,46797,46798,46802,46803,46804,46805,46806,46807,46808,46809,46814,46815,46816,46818,46819,46825,46835,46845);
  
-   $index = binary_search($allenzips, $zip, function(int $left, int $right) {
+   $index = binary_search($allenzips, $zip, function(int $left, int $right) { // closure returns: -1. 0 or 1.
        if ($left == $right)
            return 0;      
        else if ($left < $right)
